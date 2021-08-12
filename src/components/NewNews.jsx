647c658a873ec5cc46776027d8f01b081d756739
AmanRajbanshi <83454711+AmanRajbanshi@users.noreply.github.com>
2021-08-12 08:15:38 +0545

    Add files via upload

---
+++ src/components/NewNews.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+//import "./LiveScore";
+//import '../style/newNews.css'
+import NewNewsCard from "./NewNewsCard";
+const NewNews = () => {
+  return (
+    <>
+
+<NewNewsCard />
+<NewNewsCard />
+<NewNewsCard />
+<NewNewsCard />
+<NewNewsCard />
+
+
+
+         </>
+  );
+};
+
+export default NewNews;
